edf3a36110f264db0d3192d9678d423445286153
Nithin K P <nithinkp0212@gmail.com>
2026-04-02 07:02:14 +0530

    Add Two Sum solutions (brute force and hashing)

--- two_sum/brute_force.py
+++
@@ -1,8 +0,0 @@
-def two_sum_brute(nums, target):
-    for i in range(len(nums)):
-        for j in range(i+1,len(nums)):
-            if nums[i] + nums[j] == target:
-                return [i, j]
-            
-target = 9
-print(two_sum_brute(nums=nums,target=target))           
