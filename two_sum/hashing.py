aacbf55e26f7acf5d23fd84f22737cac80892778
Nithin K P <nithinkp0212@gmail.com>
2026-04-02 06:49:38 +0530

    Add Two Sum solutions (brute force and hashing)

---
+++ two_sum/hashing.py
@@ -0,0 +1,10 @@
+def two_sum_hashing(nums, target):
+    num_map = {}
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[nums[i]] = i
+nums = [2, 7, 11, 15]
+target = 17
+print(two_sum_hashing(nums, target))        
