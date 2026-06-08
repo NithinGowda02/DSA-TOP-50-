5121e45d1407a8f07fc70635907af5eb10f8255a
Nithin K P <nithinkp0212@gmail.com>
2026-06-08 07:49:08 +0530

    Added Find Dupliacte Elements in the List problem

---
+++ duplicate_elements.py
@@ -0,0 +1,11 @@
+def find_duplicates(nums):
+    seen = set()
+    duplicates = []
+    for num in nums:
+        if num in seen:
+            duplicates.append(num)
+        else:
+            seen.add(num)
+    return duplicates
+
+print(find_duplicates([1, 2, 3, 1, 2, 5, 4, 6, 5, 4, 3]))
