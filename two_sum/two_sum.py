d8c456efc3f8fe82e227ab742ac1634791108c38
Nithin K P <nithinkp0212@gmail.com>
2026-06-08 07:07:42 +0530

    Added two_sum brute force problem

--- two_sum/two_sum.py
+++ two_sum/two_sum.py
@@ -1,9 +1,8 @@
-def two_sum_brute(nums, target):
+def Two_sum_brute(nums, target):
     for i in range(len(nums)):
         for j in range(i+1,len(nums)):
             if nums[i] + nums[j] == target:
                 return [i, j]
-            
-target = 9
-nums = [2, 7, 11, 15]
-print(two_sum_brute(nums=nums,target=target))           
+
+print(Two_sum_brute([2,7,11,15], 13))            
+        
