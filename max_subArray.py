1f2075c9ab2232f5b098da3ba6b06070c3bb0a74
Nithin K P <nithinkp0212@gmail.com>
2026-06-09 06:52:11 +0530

    Maximum SubArray problem

---
+++ max_subArray.py
@@ -0,0 +1,8 @@
+def max_subArray(nums):
+    max_sum = current_sum = nums[0]
+    for num in nums[1:]:
+        current_sum = max(num, current_sum+num)
+        max_sum = max(max_sum, current_sum)
+    return max_sum
+
+print(max_subArray([-2,1,-3,4,-1,2,1,-5,4]))    
