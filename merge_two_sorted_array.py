144a3d1e184ec3c3d6cddc866f43a6bdbc3f9bea
Nithin K P <nithinkp0212@gmail.com>
2026-06-10 07:05:43 +0530

    Merge two sorted Array problem

---
+++ merge_two_sorted_array.py
@@ -0,0 +1,15 @@
+def merge_sort(a, b):
+    i = j = 0
+    result = []
+    while i < len(a) and j < len(b):
+        if a[i] <= b[j]:
+            result.append(a[i])
+            i+=1
+        else:
+            result.append(b[j])
+            j+=1
+    result.extend(a[i:])
+    result.extend(b[j:])
+    return result
+
+print(merge_sort([1,3,5],[2,4,6]))            
