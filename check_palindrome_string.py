4e517eea77bd70b6c377d510c716e3b7969c4a99
Nithin K P <nithinkp0212@gmail.com>
2026-06-11 06:38:43 +0530

    added palindrome check problem

---
+++ check_palindrome_string.py
@@ -0,0 +1,13 @@
+def palindrome(s):
+    s = ''.join(c.lower() for c in s if c.isalnum())
+    l = 0
+    r = len(s)-1
+    while l < r:
+        if s[l] != s[r]:
+            return False
+        l+=1
+        r-=1
+    return True
+
+
+print(palindrome("A man a plan a canal Panama"))
