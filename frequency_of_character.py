6163314fbbe998620f81dd8d8cadbadc4ac16d9c
Nithin K P <nithinkp0212@gmail.com>
2026-06-17 10:56:08 +0530

    frequency of character

---
+++ frequency_of_character.py
@@ -0,0 +1,11 @@
+def frequency(s):
+    character_dict = {}
+    for char in s:
+        if char in character_dict:
+            character_dict[char] += 1
+        else:
+            character_dict[char] = 1
+    return character_dict
+print(frequency("programming"))            
+
+
