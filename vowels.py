700e46d159dc0513a466ca1e9dcbc317ff5ec497
Nithin K P <nithinkp0212@gmail.com>
2026-06-20 15:14:50 +0530

    Vowels Count

---
+++ vowels.py
@@ -0,0 +1,13 @@
+def vowel(s):
+    vowels = ['a','e','i','o','u']
+    count = {}
+    for i in s.lower():
+        if i in vowels:
+            if i in count:
+                count[i] += 1
+            else:
+                count[i] = 1    
+
+    return count
+print(vowel("helloe"))        
+
