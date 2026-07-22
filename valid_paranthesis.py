04dd48729b8ae3b63ed103a0c02111534b5242d3
Nithin K P <nithinkp0212@gmail.com>
2026-07-22 11:16:20 +0530

    Added Find valid parenthesis

--- valid_paranthesis.py
+++ valid_paranthesis.py
@@ -1,15 +1,27 @@
+# def valid_parenthesis(s):
+#     stack = []
+#     mapping = {')':'(', ']':'[', '}':'{'}
+#     for char in s:
+#         if char in mapping:
+#             top = stack.pop() if stack else '#'
+#             if mapping[char] != top:
+#                 return False
+#         else:
+#             stack.append(char) 
+
+#     return not stack
+
+# print(valid_parenthesis("(){}[]"))
+# print(valid_parenthesis("({[]})"))           
+
 def valid_parenthesis(s):
     stack = []
     mapping = {')':'(', ']':'[', '}':'{'}
     for char in s:
         if char in mapping:
-            top = stack.pop() if stack else '#'
-            if mapping[char] != top:
+            if not stack or stack.pop() != mapping[char]:
                 return False
-        else:
-            stack.append(char) 
-
-    return not stack
-
-print(valid_parenthesis("(){}[]"))
-print(valid_parenthesis("({[]})"))           
+        else:    
+            stack.append(char)
+    return len(stack) == 0
+print(valid_parenthesis("([))"))        
